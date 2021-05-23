58989485220249a14fd1b97d5d4c8199ff0c4521
Glenn Arkell <glennarkell@gmail.com>
2021-05-23 11:25:37 +1000

    Update voterform.php
    Added logic to allow for notifications to proxy voters

--- site/models/voterform.php
+++ site/models/voterform.php
@@ -285,12 +285,18 @@ class GavotingModelVoterForm extends FormModel
         $proxy_type = $params->get('proxy_type'); // 0=general,1=specific,2=hybrid
         $dupl_vote = $params->get('dupl_vote');   //0=no,1=yes  only needs to be consider if proxy_type = 0
 
-		//get all the nomination ids from the checked data fields
-		$nom_ids = array();
-		foreach ($data['nom_ids'] as $keys => $values) {
-			$pos_ids[substr($keys,6)] = $values;
-		}
-		$data['votes'] = $pos_ids;
+	//get all the nomination ids from the checked data fields
+	// keys to nom_ids is the position id ref and value is the monination id ref
+	$pos_ids = array();
+	$voting = array();
+	foreach ($data['nom_ids'] as $keys => $values) {
+		$pos_ids[substr($keys,6)] = $values;
+		$pos_name = GavotingHelper::getPosition(substr($keys,6))->position_name;
+		$nom = GavotingHelper::getNomination($values);
+		foreach ($nom as $n) { $nom_name = $n->nomination_name; }
+		$voting[$pos_name] = $nom_name;
+	}
+	$data['votes'] = $pos_ids;
 
         if ($authorised !== true) {
             Factory::getApplication()->enqueueMessage(Text::_('COM_GAVOTING_NOTAUTH_VOTE'), 'danger');
@@ -300,57 +306,63 @@ class GavotingModelVoterForm extends FormModel
         $vote = array('id'=>0,'state'=>1,'user_id'=>$data['user_id'],'proxy_vote'=>0,'cat_id'=>$data['cat_id']);
         $mu = Factory::getUser($data['user_id']);
         // check for proxy settings
-		if ($data['proxy_vote']) {
-			$pu = Factory::getUser($data['proxy_for']);
+	if ($data['proxy_vote']) {
+		$pu = Factory::getUser($data['proxy_for']);
 	        $hasVoted = GavotingHelper::hasVoted($pu->id);
 	        if ($hasVoted) {
 	            $app->enqueueMessage(Text::_('COM_GAVOTING_NOMINATIONS_ALREADY_VOTED'), 'danger');
 	            return false;
 	        }
-			$proxyvote = array('id'=>0,'state'=>1,'user_id'=>$data['proxy_for'],'proxy_vote'=>1,'cat_id'=>$data['cat_id']);
-			// specific and hybrid means only record vote for proxy_for
-			if ($proxy_type == 1 || $proxy_type == 2) {
-				// Set voteform data based on selected proxy
+		$proxyvote = array('id'=>0,'state'=>1,'user_id'=>$data['proxy_for'],'proxy_vote'=>1,'cat_id'=>$data['cat_id']);
+		// specific and hybrid means only record vote for proxy_for
+		if ($proxy_type == 1 || $proxy_type == 2) {
+			// Set voteform data based on selected proxy
+			$ptable = $this->getTable();
+                	if ($ptable->save($proxyvote) === true) {
+				GavotingHelper::addVoteToNomination($data['votes']);
+			} else {
+				$app->enqueueMessage(Text::sprintf('COM_GAVOTING_PROXY_VOTE_FOR_FAILED',$mu->name,$pu->name), 'warning');
+			}
+		} else {
+			// general means test for dupl_vote
+			if ($dupl_vote) {
+				// save twice
 				$ptable = $this->getTable();
-                if ($ptable->save($proxyvote) === true) {
+                    		if ($ptable->save($proxyvote) === true) {
 					GavotingHelper::addVoteToNomination($data['votes']);
+		                	if ($params->get('notif_proxy',0)) {
+						GavotingHelper::sendVotingEmail($mu, $pu, $voting, $params);
+					}
 				} else {
 					$app->enqueueMessage(Text::sprintf('COM_GAVOTING_PROXY_VOTE_FOR_FAILED',$mu->name,$pu->name), 'warning');
 				}
-			} else {
-				// general means test for dupl_vote
-				if ($dupl_vote) {
-					// save twice
-					$ptable = $this->getTable();
-                    if ($ptable->save($proxyvote) === true) {
-						GavotingHelper::addVoteToNomination($data['votes']);
-					} else {
-						$app->enqueueMessage(Text::sprintf('COM_GAVOTING_PROXY_VOTE_FOR_FAILED',$mu->name,$pu->name), 'warning');
-					}
 
-					$table = $this->getTable();
+				$table = $this->getTable();
 			        if ($table->save($vote) === true) {
 				        GavotingHelper::addVoteToNomination($data['votes']);
 			        } else {
-						$app->enqueueMessage(Text::sprintf('COM_GAVOTING_MEMBER_VOTE_FOR_FAILED',$mu->name), 'warning');
+					$app->enqueueMessage(Text::sprintf('COM_GAVOTING_MEMBER_VOTE_FOR_FAILED',$mu->name), 'warning');
 			        }
-				} else {
-					$ptable = $this->getTable();
-                    if ($ptable->save($proxyvote) === true) {
+			} else {
+				$ptable = $this->getTable();
+                		if ($ptable->save($proxyvote) === true) {
 				        GavotingHelper::addVoteToNomination($data['votes']);
-					} else {
-						$app->enqueueMessage(Text::sprintf('COM_GAVOTING_MEMBER_VOTE_FOR_FAILED',$mu->name), 'warning');
+		                	if ($params->get('notif_proxy',0)) {
+						GavotingHelper::sendVotingEmail($mu, $pu, $voting, $params);
 					}
-
+				} else {
+					$app->enqueueMessage(Text::sprintf('COM_GAVOTING_MEMBER_VOTE_FOR_FAILED',$mu->name), 'warning');
 				}
+
 			}
+		}
 		} else {
 
 	        $table = $this->getTable();
 	        if ($table->save($vote) === true) {
 		        GavotingHelper::addVoteToNomination($data['votes']);
 	        } else {
-				$app->enqueueMessage(Text::sprintf('COM_GAVOTING_MEMBER_VOTE_FOR_FAILED',$mu->name), 'warning');
+			$app->enqueueMessage(Text::sprintf('COM_GAVOTING_MEMBER_VOTE_FOR_FAILED',$mu->name), 'warning');
 	        }
         }
 
